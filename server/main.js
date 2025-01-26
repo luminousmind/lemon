463b5692f417887e9956abf1864bdbee631b6035
luminousmind <193550240+luminousmind@users.noreply.github.com>
2025-01-27 10:51:57 +1100

    feat: configure session middleware and middleware to parse HTTP

--- server/main.js
+++ server/main.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const session = requrie("express-session")
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// Middleware to parse HTTP responses
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Configuration for session middleware
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    },
+    resave: true,
+    saveUninitialized: false,
+    secure: process.env.NODE_ENV == "production"
+}));
 
 app.listen(PORT, () => {
     console.log("Server is running at", PORT);
